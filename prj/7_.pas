8bb7ada1759c8877df1d732aeb8ecd74b34974b7
stebo000000 <3367977@gmail.com>
2024-11-21 09:34:37 +0300

    #

--- prj/7_.pas
+++ prj/7_.pas
@@ -11,6 +11,10 @@ var
     f: TextFile;
     ft: file of real;
     el: real;
+    el1: real;
+    pos: integer;
+    is_exists: boolean := false;
+    is_end: boolean;
 
 procedure print_typed_file(ft: file of real);
 begin
@@ -19,6 +23,41 @@ begin
     begin
         read(ft, el);
         writeln(el)
+    end;
+end;
+
+procedure find_right_el(ft: file of real);
+begin
+Seek(ft, pos);
+    begin
+        read(ft, el);
+        if el < 0 then
+        begin
+            is_exists := true;
+        end;
+        if (pos > 0) and (not is_exists) then
+        begin
+            pos -= 1;
+            find_right_el(ft);
+        end;
+    end;
+end;
+
+function replace_elements(ft: file of real):boolean;
+begin
+    Seek(ft, pos);
+    if pos <> fileSize(ft)-1 then
+    begin
+        read(ft, el);
+        read(ft, el1);
+        Seek(ft, pos);
+        write(ft, el1);
+        write(ft, el);
+        result := false
+    end
+    else
+    begin
+        result := true
     end;
 end;
 
@@ -43,8 +82,23 @@ begin
             end;
 
             print_typed_file(ft);
-        end;
+            writeln;
+            pos:= fileSize(ft)-1;
+            find_right_el(ft);
+            if is_exists then
+            begin
+                write('element: ', el, ' at pos: ', pos+1);
+                writeln;
+                writeln;
 
-        
+                while (not is_end) do
+                begin
+                    is_end := replace_elements(ft);
+                    pos += 1
+                end;
+
+                print_typed_file(ft)
+            end;
+        end;        
 end;
 end.
