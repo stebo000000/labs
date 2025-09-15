aa41ea7ee84edf57ae5af435a920ef66c05da34c
stebo000000 <3367977@gmail.com>
2025-09-15 23:45:30 +0300

    #

--- cpp-labs/lab2-d.cpp
+++ cpp-labs/lab2-d.cpp
@@ -7,20 +7,21 @@ int main(int argc, const char* argv[])
     FILE* fptr;
     fptr = fopen(argv[1], "r");
 
-    float** matrix1, matrix2;
+    float** matrix1;
+    float** matrix2;
 
     int colLen1, colLen2, rowLen1, rowLen2;
 
     input(fptr, colLen1, colLen2);
     input(fptr, rowLen1, rowLen2);
 
-    printMatrix(matrix1);
-    printMatrix(matrix2);
+    printMatrix(matrix1, colLen1, rowLen1);
+    printMatrix(matrix2, colLen2, rowLen2);
 
     fclose(fptr);
 
-    float mean1 = findMean(matrix1);
-    float mean2 = findMean(matrix2);
+    float mean1 = findMean(matrix1, colLen1, rowLen1);
+    float mean2 = findMean(matrix2, colLen2, rowLen2);
 
     float prod;
 
@@ -28,30 +29,30 @@ int main(int argc, const char* argv[])
     {
         if (mean1 == mean2)
         {
-            prod = findProduct(matrix1);
+            prod = findProduct(matrix1, colLen1, rowLen1);
             printf("%f", prod);
-            prod = findProduct(matrix2);
+            prod = findProduct(matrix2, colLen2, rowLen2);
             printf("%f", prod);
         }
         else if (mean1 < mean2)
         {
-            prod = findProduct(matrix1);
+            prod = findProduct(matrix1, colLen1, rowLen1);
             printf("%f", prod);
         }
         else
         {
-            prod = findProduct(matrix2);
+            prod = findProduct(matrix2, colLen2, rowLen2);
             printf("%f", prod);
         }
     }
     else if (mean1)
     {
-        prod = findProduct(matrix1);
+        prod = findProduct(matrix1, colLen1, rowLen1);
         printf("%f", prod);
     }
     else if (mean2)
     {
-        prod = findProduct(matrix2);
+        prod = findProduct(matrix2, colLen2, rowLen2);
         printf("%f", prod);
     }
     else
@@ -59,8 +60,8 @@ int main(int argc, const char* argv[])
         printf("no right mean");
     }
 
-    freeMem(matrix1);
-    freeMem(matrix2);
+    freeMem(matrix1, colLen1, rowLen1);
+    freeMem(matrix2, colLen2, rowLen2);
     
     return 0;
 }
