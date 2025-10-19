fbad4e1d45b736c22128cd8c101df538df263506
stebo000000 <3367977@gmail.com>
2025-10-19 21:53:06 +0300

    #

--- ed/lab4.py
+++ ed/lab4.py
@@ -11,71 +11,92 @@ def is_empty(lst: list):
 
 def add_first(lst: list[str], el: str):
     length = len(lst)
-    new_lst = [""] * (length + 1)
+    new_lst = [["", 0]] * (length + 1)
 
-    new_lst[0] = el
+    new_lst[0][0] = el
+    if length > 0:
+        new_lst[0][1] = 1
+    else:
+        new_lst[0][1] = -1
 
     for i in range(1, length):
-        new_lst[i] = lst[i - 1]
+        new_lst[i][0] = lst[i - 1][0]
+        new_lst[i][1] = lst[i - 1][1] + 1
 
     return new_lst
 
 
 def add_last(lst: list[str], el: str):
     length = len(lst)
-    new_lst = [""] * (length + 1)
+    new_lst = [["", 0] for _ in range(length + 1)]
 
-    for i in range(length):
-        new_lst[i] = lst[i]
+    if is_empty(lst):
+        new_lst[length][0] = el
+        new_lst[length][1] = -1
+    else:
+        for i in range(length - 1):
+            new_lst[i][0] = lst[i][0]
+            new_lst[i][1] = lst[i][1]
 
-    new_lst[length] = el
+        new_lst[length - 1][0] = lst[length - 1][0]
+        new_lst[length - 1][1] = length
+
+        new_lst[length][0] = el
+        new_lst[length][1] = -1
 
     return new_lst
 
 
 def add_after_currnet(lst: list[str], current: int, el: str):
     length = len(lst)
-    new_lst = [""] * (length + 1)
+    new_lst = [["", 0]] * (length + 1)
 
     for i in range(current):
-        new_lst[i] = lst[i]
+        new_lst[i][0] = lst[i][0]
+        new_lst[i][1] = lst[i][1]
     
-    new_lst[current] = el
+    new_lst[current][0] = lst[current][0]
+    new_lst[current][1] = current + 1
+
+    new_lst[current + 1][0] = el
+    new_lst[current + 1][1] = current + 2
 
     for i in range(current, length):
-        new_lst[i + 1] = lst[i]
+        new_lst[i + 1][0] = lst[i][0]
+        new_lst[i + 1][1] = lst[i][1] + 1
         
     return new_lst
 
 
 def get_first(lst: list[str]):
     if not is_empty(lst):
-        return lst[0]
+        return lst[0][0]
     else:
-        return None
+        return []
 
 
 def get_last(lst: list[str]):
     if not is_empty(lst):
-        return lst[-1]
+        return lst[-1][0]
     else:
-        return None
+        return []
 
 
 def get_current(lst: list[str], current: int):
     if not is_empty(lst):
-        return lst[current]
+        return lst[current][0]
     else:
-        return None
+        return []
     
 
 def del_first(lst: list[str]):
     if not is_empty(lst):
         length = len(lst)
-        new_lst = [""] * (length - 1)
+        new_lst = [["", 0]] * (length - 1)
 
         for i in range(1, length):
-            new_lst[i - 1] = lst[i]
+            new_lst[i - 1][0] = lst[i][0]
+            new_lst[i - 1][1] = lst[i][1] - 1
     else:
         new_lst = []
 
@@ -85,10 +106,14 @@ def del_first(lst: list[str]):
 def del_last(lst: list[str]):
     if not is_empty(lst):
         length = len(lst)
-        new_lst = [""] * (length - 1)
+        new_lst = [["", 0]] * (length - 1)
 
-        for i in range(length - 1):
-            new_lst[i] = lst[i]
+        for i in range(length - 2):
+            new_lst[i][0] = lst[i][0]
+            new_lst[i][1] = lst[i][1]
+
+        new_lst[length - 2][0] = lst[length - 2][0]
+        new_lst[length - 2][1] = -1
     else:
         new_lst = []
 
@@ -98,22 +123,24 @@ def del_last(lst: list[str]):
 def del_current(lst: list[str], current: int):
     length = len(lst)
     
-    new_lst = [0.0] * (length - 1)
+    new_lst = [["", 0]] * (length - 1)
 
-    for i in range(current):
-        new_lst[i] = lst[i]
+    for i in range(current - 1):
+        new_lst[i][0] = lst[i][0]
+        new_lst[i][1] = lst[i][1]
 
     for i in range(current + 1, length):
-        new_lst[i - 1] = lst[i]
+        new_lst[i - 1][0] = lst[i][0]
+        new_lst[i - 1][1] = lst[i][1] - 1
 
     return new_lst
 
 
 def next_el(lst: list[str], current: int):
-    if current < len(lst) - 1:
-        current += 1
+    if current < len(lst):
+        current = lst[current][1]
     else:
-        current = None
+        current = -1
     return current
 
 
@@ -132,7 +159,8 @@ def input_lst(name: str):
 
 def output(lst: list[str], f: typing.TextIO):
     i = 0
-    while i is not None:
+    while i != -1:
+        print(1, i)
         f.write(str(get_current(lst, i)) + " ")
         i = next_el(lst, i)
 
@@ -141,7 +169,8 @@ def count(lst: list[str], target: int):
     c = 0
     i = 0
 
-    while i is not None:
+    while i != -1:
+        print(2, i)
         if len(get_current(lst, i)) == target:
             c += 1
         i = next_el(lst, i)
