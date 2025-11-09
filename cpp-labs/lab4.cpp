bfba7e0d55380d9ac85407eab8e09296ab6189be
stebo000000 <3367977@gmail.com>
2025-11-10 01:07:28 +0300

    #

--- cpp-labs/lab4.cpp
+++ cpp-labs/lab4.cpp
@@ -1,64 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-bool isCorrectChar(unsigned char chr) {
-    return chr >= '0' && chr <= '9' || chr >= 'a' && chr <= 'z' || chr >= 'A' && chr <= 'Z';
-}
-
+bool isCorrectChar(unsigned char chr) {return chr >= '0' && chr <= '9' || chr >= 'a' && chr <= 'z' || chr >= 'A' && chr <= 'Z';}
 void readWord(FILE* fptr, char*& str, int& wasRead, unsigned char& chr) {
-    do
-    {
-        chr = fgetc(fptr);
-    } while (!isCorrectChar(chr) && chr != 255);
-    
-
-    while (isCorrectChar(chr) && chr != 255)
-    {
+    str = (char*)malloc(sizeof(char));
+    do {chr = fgetc(fptr);} while (!isCorrectChar(chr) && chr != 255);
+    while (isCorrectChar(chr) && chr != 255) {
         str[wasRead++] = chr;
         str = (char*)realloc(str, sizeof(char) * wasRead + 1);
         chr = fgetc(fptr);
     }
-    //! probably wasRead--;
     return;
 }
-
 void readWords(const char* fileName, char**& strs, int*& wordsLens, int& wordsCount) {
     FILE* fptr1;
     unsigned char chr = ' ';
     int wasRead;
     wordsLens = (int*)malloc(sizeof(int));
-
     fptr1 = fopen(fileName, "r");
-
-    while (chr != 255)
-    {
-        while (chr != 255)
-        {
+    while (chr != 255) {
+        while (chr != 255) {
             wasRead = 0;
-            readWord(fptr1, strs[wordsCount++], wasRead, chr);
-            wordsLens = (int*)realloc(wordsLens, sizeof(int) * wordsCount + 1);
-            strs = (char**)realloc(strs, sizeof(int) * wordsCount + 1);
-
-        }
-    }
-    //! probably wordsCount--;
-
+            readWord(fptr1, strs[wordsCount], wasRead, chr);
+            if (wasRead > 0) {
+                wordsLens[wordsCount++] = wasRead;
+                wordsLens = (int*)realloc(wordsLens, sizeof(int) * wordsCount + 1);
+                strs = (char**)realloc(strs, sizeof(char*) * wordsCount + 1);
+            }}}
     fclose(fptr1);
     return;
 }
-
-
-void getWordsIndexes(int strLen, int* wordIndexes) {
-    for (int i = 0; i < strLen; i++)
-    {
-        wordIndexes[i] = i;
-    }
-    return;
-}
-
 void sort(int* wordsLens, int& wordsCount, int* wordIndexes) {
     int buffer;
-
     for (int i = 0; i < wordsCount - 1; i++)
     {
         for (int j = i + 1; j < wordsCount; j++)
@@ -68,28 +40,21 @@ void sort(int* wordsLens, int& wordsCount, int* wordIndexes) {
                 buffer = wordsLens[i];
                 wordsLens[i] = wordsLens[j];
                 wordsLens[j] = buffer;
-
                 buffer = wordIndexes[i];
                 wordIndexes[i] = wordIndexes[j];
                 wordIndexes[j] = buffer;
-            }
-        }
-    }
-    
+            }}}
     return;
 }
-
-
 int main(int argc, char const *argv[])
 {  
     char** strs = (char**)malloc(sizeof(char*));
     int* wordsLens;
-    int wordsCount;
+    int wordsCount = 0;
     readWords(argv[1], strs, wordsLens, wordsCount);
-    
-    int* wordIndexes = (int*)malloc(sizeof(int));
+    int* wordIndexes = (int*)malloc(sizeof(int) * wordsCount);
+    for (int i = 0; i < wordsCount; i++) wordIndexes[i] = i;
     sort(wordsLens, wordsCount, wordIndexes);
-
     for (int i = 0; i < wordsCount; i++)
     {
         for (int j = 0; j < wordsLens[i]; j++)
@@ -98,6 +63,11 @@ int main(int argc, char const *argv[])
         }
         printf("\n");
     }
-    
+    for (int i = 0; i < wordsCount; i++)
+    {
+        free(strs[i]);
+    }
+    free(wordIndexes);
+    free(wordsLens);
     return 0;
 }
