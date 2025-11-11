094f54c2e41b76a91d751bddbb62c96db14f7222
stebo000000 <3367977@gmail.com>
2025-11-11 10:58:19 +0300

    #

--- cpp-labs/lab4.cpp
+++ cpp-labs/lab4.cpp
@@ -1,57 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 bool isCorrectChar(unsigned char chr) {return chr >= '0' && chr <= '9' || chr >= 'a' && chr <= 'z' || chr >= 'A' && chr <= 'Z';}
-void readWord(FILE* fptr, char*& str, int& wasRead, unsigned char& chr) {
-    str = (char*)malloc(sizeof(char));
-    do {chr = fgetc(fptr);} while (!isCorrectChar(chr) && chr != 255);
-    while (isCorrectChar(chr) && chr != 255) {
+void readGarbage(FILE* fptr, char*& str, unsigned char& chr){
+    chr = fgetc(fptr);
+    if (!isCorrectChar(chr) && chr != 255) readGarbage(fptr, str, chr);
+    return;
+}
+void readLetter(FILE* fptr, char*& str, int& wasRead, unsigned char& chr) {
+    if (isCorrectChar(chr) && chr != 255) {
         str[wasRead++] = chr;
         str = (char*)realloc(str, sizeof(char) * wasRead + 1);
         chr = fgetc(fptr);
-    }
+        readLetter(fptr, str, wasRead, chr);}
     return;
 }
-void readWords(const char* fileName, char**& strs, int*& wordsLens, int& wordsCount) {
-    FILE* fptr1 = fopen(fileName, "r");
+void readWord(FILE* fptr, char*& str, int& wasRead, unsigned char& chr) {
+    str = (char*)malloc(sizeof(char));
+    readGarbage(fptr, str, chr);
+    readLetter(fptr, str, wasRead, chr);
+    return;
+}
+void readWords(FILE* fptr, char**& strs, int*& wordsLens, int& wordsCount) {
     unsigned char chr = ' ';
     int wasRead;
-    while (chr != 255) {
-        while (chr != 255) {
-            wasRead = 0;
-            readWord(fptr1, strs[wordsCount], wasRead, chr);
-            if (wasRead > 0) {
-                wordsLens[wordsCount++] = wasRead;
-                wordsLens = (int*)realloc(wordsLens, sizeof(int) * wordsCount + 1);
-                strs = (char**)realloc(strs, sizeof(char*) * wordsCount + 1);
-            }}}
-    fclose(fptr1);
+    if (chr != 255) {
+        wasRead = 0;
+        readWord(fptr, strs[wordsCount], wasRead, chr);
+        if (wasRead > 0) {
+            wordsLens[wordsCount++] = wasRead;
+            wordsLens = (int*)realloc(wordsLens, sizeof(int) * wordsCount + 1);
+            strs = (char**)realloc(strs, sizeof(char*) * wordsCount + 1);
+            readWords(fptr, strs, wordsLens, wordsCount);}}
     return;
 }
-void sort(int* wordsLens, int& wordsCount, char** strs) {
+void sort(int* wordsLens, int& wordsCount, char** strs, int i, int j) {
     int buffer;
     char* buf;
-    for (int i = 0; i < wordsCount - 1; i++) {
-        for (int j = i + 1; j < wordsCount; j++) {
+    if (i < wordsCount - 1) {
+        if (j < wordsCount) {
+            // printf("%d, %d\n", i, j);
             if (wordsLens[i] < wordsLens[j]) {
                 buffer = wordsLens[i];
                 wordsLens[i] = wordsLens[j];
                 wordsLens[j] = buffer;
                 buf = strs[i];
                 strs[i] = strs[j];
-                strs[j] = buf;
-            }}}
+                strs[j] = buf;}
+            sort(wordsLens, wordsCount, strs, i, ++j);}
+        sort(wordsLens, wordsCount, strs, ++i, i + 1);}
     return;
 }
-int main(int argc, char const *argv[]) {  
+int main(int argc, char const *argv[]) {
     char** strs = (char**)malloc(sizeof(char*));
     int* wordsLens = (int*)malloc(sizeof(int));
     int wordsCount = 0;
-    readWords(argv[1], strs, wordsLens, wordsCount);
-    sort(wordsLens, wordsCount, strs);
+    FILE* fptr = fopen(argv[1], "r");
+    readWords(fptr, strs, wordsLens, wordsCount);
+    fclose(fptr);
+    printf("%d\n", wordsCount);
+    sort(wordsLens, wordsCount, strs, 0, 1);
     for (int i = 0; i < wordsCount; i++) {
         for (int j = 0; j < wordsLens[i]; j++) printf("%c", strs[i][j]);
-        printf("\n");
-    }
+        printf("\n");}
     for (int i = 0; i < wordsCount; i++) free(strs[i]);
     free(wordsLens);
     return 0;
--- cpp-labs/lab7.cpp
+++ cpp-labs/lab7.cpp
@@ -1,18 +1,81 @@
 #include <stdio.h>
+#define INT_SIZE 8
 
+#if INT_SIZE == 8
+typedef unsigned char MYINT;
+#endif
+#if INT_SIZE == 16
+typedef unsigned short MYINT;
+#endif
+#if INT_SIZE == 32
+typedef unsigned int MYINT;
 
-int main(int argc, char const *argv[]) {
-    unsigned int x, n, p;
+#endif
+#if INT_SIZE == 64
+typedef unsigned long MYINT;
+#endif
+
+void printBin(MYINT x) {
+    printf("bin: ");
+    for (int i = sizeof(MYINT) * 8 - 1; i >= 0; i--)
+    {
+        printf("%d", (x >> i) & 1);
+    }
+    printf("\n");
+    return;
+}
+
+void printHex(MYINT x) {
+    char hexDigits[] = "0123456789ABCDEF";
+    char buffer[INT_SIZE / 4];
+    int i = 0;
+    printf("hex: ");
+
+    while (x >= 1) {
+        buffer[i] = hexDigits[x % 16];
+        x /= 16;
+        i++;
+    }
+
+    while (i > 0) {
+        printf("%c", buffer[--i]);
+    }
+    printf("\n");
+
+    return;
+}
+
+void printDec(MYINT x) {
+    printf("dec: ");
+    printf("%d\n", x);
+    return;
+}
+
+int main() {
+    MYINT x;
+    int n, p;
 
     printf("x n p\n");
 
-    scanf("%d %d %d", &x, &n, &p);
+    scanf("%ld %ld %ld", &x, &n, &p);
 
-    unsigned int mask = ~(1 << n) << p; // ~0
+    if (INT_SIZE <= n + p)
+    {
+        printf("incorrect params");
+    }
+    else
+    {
+        MYINT mask = ~(~0 << n) << p << 1;
 
-    unsigned int result = x | mask;
+        // printBin(x);
+        // printBin(mask);
 
-    printf("", result);
+        int result = x | mask;
+
+        printDec(result);
+        printHex(result);
+        printBin(result);
+    }
 
     return 0;
 }
