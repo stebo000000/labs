e9d0ab938ad6cc3223518cc97e65e047f8bcb7ee
stebo000000 <stebo000000@waveteam.net>
2026-02-26 11:33:05 +0300

    #

--- cpp-labs/lab1/main.cpp
+++ cpp-labs/lab1/main.cpp
@@ -9,6 +9,7 @@ int main() {
     switch (userInput) {
     case 'a': {
       float coefficient, e_pow, x_pow;
+      std::cout << "k " << "e_pow " << "x_pow\n";
       std::cin >> coefficient >> e_pow >> x_pow;
       polynomial.add(Term(coefficient, e_pow, x_pow));
       break;
@@ -32,6 +33,10 @@ int main() {
     case 'h': {
       std::cout << "h - help\na - add\np - print\ni - integrate\nq - quit\nf - "
                    "from file\n";
+      break;
+    }
+    default: {
+      std::cout << "Unknown command\n h - for help\n";
       break;
     }
     }
